36b256b5a0862056f75777a56a52038a0823a838
patel salar <patel.salar@skillassure.com>
2024-01-16 12:07:05 +0530

    bbok

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
 })
 export class BooksComponent {
 
-
+src: string ="https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg"
   name = 'clean code'
   author = 'salaar patel'
+
+  name2 :string = "Pragmatic Programmer "
+  author2:string='David Thomas'
+  src2:string='https://m.media-amazon.com/images/I/912PFdNLs-L._AC_UF1000,1000_QL80_.jpg'
+
+  isDisabled:boolean=false
 }
